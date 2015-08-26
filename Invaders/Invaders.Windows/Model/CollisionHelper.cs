e781f4bdcc757d397b6b71f7d42f57c0ff2701bf
Rafal <rafal.araszkiewicz@student.put.poznan.pl>
2015-08-26 23:06:28 +0200

    #0.0.71 CollisionHelper static class added.

---
+++ Invaders/Invaders.Windows/Model/CollisionHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.Foundation;
+
+namespace Invaders.Model
+{
+    internal static class CollisionHelper
+    {
+        internal static bool CheckCollision(Rect area, Point location)
+        {
+            return area.Contains(location);
+        }
+
+        internal static bool CheckCollision(Rect rect1, Rect rect2)
+        {
+            rect1.Intersect(rect2);
+            return rect1.Height > 0 && rect1.Width > 0 ? true : false;
+        }
+    }
+}
