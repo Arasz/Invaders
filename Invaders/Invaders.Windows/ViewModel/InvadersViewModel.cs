3fa9b869737f46c97ca061e6b97c8a4b3bf5ae78
Rafal <rafal.araszkiewicz@student.put.poznan.pl>
2015-08-27 17:42:33 +0200

    #0.0.75 InvadersPage added.

--- Invaders/Invaders.Windows/ViewModel/InvadersViewModel.cs
+++ Invaders/Invaders.Windows/ViewModel/InvadersViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace Invaders.ViewModel
 {
     class InvadersViewModel
     {
+        public ObservableCollection<BitmapImage> Sprites { get; private set; }
     }
 }
